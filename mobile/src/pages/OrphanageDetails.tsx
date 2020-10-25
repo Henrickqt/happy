27cfddfe83a60f5d5aeadd13f3c0bf182b7f4ad0
Henrickqt <pedro.msg@hotmail.com>
2020-10-25 13:36:24 -0200

    Create the OrphanageDetails screen

--- mobile/src/pages/OrphanageDetails.tsx
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -1,15 +1,107 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather, FontAwesome } from '@expo/vector-icons';
+
+import mapMarker from '../images/map-marker.png';
 
 import styles from '../styles/pages/orphanage-details';
 
 function OrphanageDetail() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hello World!
-      </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.imagesContainer}>
+        <ScrollView horizontal pagingEnabled>
+          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
+          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
+          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
+        </ScrollView>
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <Text style={styles.title}>
+          Orf. Esperança
+        </Text>
+        <Text style={styles.description}>
+          Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.
+        </Text>
+
+        <View style={styles.mapContainer}>
+          <MapView 
+            style={styles.mapStyle} 
+            provider={PROVIDER_GOOGLE} 
+            initialRegion={{
+              latitude: -21.2284996,
+              longitude: -45.0093355,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }} 
+            zoomEnabled={false} 
+            pitchEnabled={false} 
+            scrollEnabled={false} 
+            rotateEnabled={false} 
+          >
+            <Marker 
+              icon={mapMarker} 
+              coordinate={{
+                latitude: -21.2284996,
+                longitude: -45.0093355,
+              }} 
+            />
+          </MapView>
+
+          <View style={styles.routesContainer}>
+            <Text style={styles.routesText}>
+              Ver rotas no Google Maps
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.separator} />
+
+        <Text style={styles.title}>
+          Instruções para visita
+        </Text>
+        <Text style={styles.description}>
+          Venha como se sentir a vontade e traga muito amor e paciência para dar.
+        </Text>
+
+        <View style={styles.scheduleContainer}>
+          <View style={[styles.scheduleItem, styles.scheduleItemBlue]}>
+            <Feather name="clock" size={40} color="#2AB5D1" />
+            <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>
+              Segunda à Sexta 8h às 18h
+            </Text>
+          </View>
+          {1==1 
+            ? (
+              <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+                <Feather name="info" size={40} color="#39CC83" />
+                <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>
+                  Atendemos fim de semana
+                </Text>
+              </View>
+            )
+            : (
+              <View style={[styles.scheduleItem, styles.scheduleItemRed]}>
+                <Feather name="info" size={40} color="#FF669D" />
+                <Text style={[styles.scheduleText, styles.scheduleTextRed]}>
+                  Não atendemos fim de semana
+                </Text>
+              </View>
+            )
+          }
+        </View>
+
+        <RectButton style={styles.contactButton} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={24} color="#FFFFFF" />
+          <Text style={styles.contactButtonText}>
+            Entrar em contato
+          </Text>
+        </RectButton>
+      </View>
+    </ScrollView>
   );
 }
 
--- mobile/src/styles/pages/orphanage-details.ts
+++ mobile/src/styles/pages/orphanage-details.ts
@@ -1,8 +1,135 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  imagesContainer: {
+    height: 240,
+  },
+
+  image: {
+    width: Dimensions.get('window').width,
+    height: 240,
+    resizeMode: 'cover',
+  },
+
+  detailsContainer: {
+    padding: 24,
+  },
+
+  title: {
+    color: '#4D6F80',
+    fontSize: 30,
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  description: {
+    marginTop: 16,
+    color: '#5C8599',
+    fontFamily: 'Nunito_600SemiBold',
+    lineHeight: 24,
+  },
+
+  mapContainer: {
+    borderRadius: 20,
+    borderWidth: 1.2,
+    borderColor: '#B3DAE2',
+    marginTop: 40,
+    backgroundColor: '#E6F7FB',
+    overflow: 'hidden',
+  },
+
+  mapStyle: {
+    width: '100%',
+    height: 150,
+  },
+
+  routesContainer: {
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  routesText: {
+    color: '#0089A5',
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  separator: {
+    width: '100%',
+    height: 0.8,
+    marginVertical: 40,
+    backgroundColor: '#D3E2E6',
+  },
+
+  scheduleContainer: {
+    marginTop: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  
+  scheduleItem: {
+    width: '48%',
+    padding: 20,
+  },
+
+  scheduleItemBlue: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: '#A1E9C5',
+    backgroundColor: '#E6F7FB',
+  },
+
+  scheduleItemGreen: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: '#A1E9C5',
+    backgroundColor: '#EDFFF6',
+  },
+
+  scheduleItemRed: {
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: '#FFBCD4',
+    backgroundColor: '#FCF0F4',
+  },
+
+  scheduleText: {
+    marginTop: 20,
+    fontFamily: 'Nunito_600SemiBold',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+
+  scheduleTextBlue: {
+    color: '#5C8599',
+  },
+
+  scheduleTextGreen: {
+    color: '#37C77F',
+  },
+
+  scheduleTextRed: {
+    color: '#FF669D',
+  },
+
+  contactButton: {
+    height: 56,
+    borderRadius: 20,
+    marginTop: 40,
+    backgroundColor: '#3CDC8C',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  contactButtonText: {
+    marginLeft: 16,
+    color: '#FFFFFF',
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
   },
 });
 
